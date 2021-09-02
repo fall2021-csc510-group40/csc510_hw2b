b7d94859ae9cb19674d1b39d890c8deabb3027af
kunalpatange <kunalpatange1111@gmail.com>
2021-09-02 13:12:54 -0400

    travis.yml

--- code/main_test.go
+++ code/main_test.go
@@ -3,10 +3,19 @@ import (
 "testing"
 )
 
+func IsPalindrome(word string) bool {
+     end := len(word)-1
+     for i:=0;i<len(word)/2 ;i++{
+       if word[i] != word[end-i]  {
+              return false
+}
+}
+return true
+}
 
-func TestisPalindrome(t *testing.T){
+func TestIsPalindrome(t *testing.T){
     word := "abcba"
-    if isPalindrome(word){
+    if !IsPalindrome(word){
 	  t.Errorf("%v is a palindrome",word)
 }
 }
