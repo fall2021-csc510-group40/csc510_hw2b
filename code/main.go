7b2ea96ece304b6da7ddaf143c1e3b7204de92fa
kunalpatange <kunalpatange1111@gmail.com>
2021-09-02 13:21:36 -0400

    modified main.go

--- code/main.go
+++ code/main.go
@@ -1,15 +1,16 @@
 package main
 
 
-func isPalindrome(word string) bool {
+func IsPalindrome(word string) bool {
      end := len(word)-1
      for i:=0;i<len(word)/2 ;i++{
        if word[i] != word[end-i]  {
-	      return false
+              return false
 }
 }
 return true
 }
+
 
 func main(){
 
--- code/main_test.go
+++ code/main_test.go
@@ -3,16 +3,6 @@ import (
 "testing"
 )
 
-func IsPalindrome(word string) bool {
-     end := len(word)-1
-     for i:=0;i<len(word)/2 ;i++{
-       if word[i] != word[end-i]  {
-              return false
-}
-}
-return true
-}
-
 func TestIsPalindrome(t *testing.T){
     word := "abcba"
     if !IsPalindrome(word){
