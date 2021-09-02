b092dd02f9be0e44b5b4cc57e7b81cc3c456f375
kunalpatange <kunalpatange1111@gmail.com>
2021-09-02 12:47:28 -0400

    main_test.go

--- code/main_test.go
+++ code/main_test.go
@@ -1,13 +1,11 @@
 package main
 import ( 
 "testing"
-"fmt"
 )
 
 
 func TestisPalindrome(t *testing.T){
     word := "abcba"
-    fmt.Println("Testing if the word %s is palindrome or not",word)
     if !isPalindrome(word){
 	  t.Errorf("%v is a palindrome",word)
 }
